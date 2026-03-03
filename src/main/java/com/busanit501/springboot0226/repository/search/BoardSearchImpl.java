4d1147f3d7912bc91bfc23e1cb18fd809f20356b
lsy3709 <lsy3709@naver.com>
2026-03-03 16:00:49 +0900

    7교시, spring data jpa, 기본 4, Querydsl , 검색 조건에 적용해보기, 예시3 자바 언어로 sql , 동적 쿼리가 작성되는지 여부 확인2

--- src/main/java/com/busanit501/springboot0226/repository/search/BoardSearchImpl.java
+++ src/main/java/com/busanit501/springboot0226/repository/search/BoardSearchImpl.java
@@ -2,6 +2,7 @@ package com.busanit501.springboot0226.repository.search;
 
 import com.busanit501.springboot0226.domain.Board;
 import com.busanit501.springboot0226.domain.QBoard;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -24,6 +25,19 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
         JPQLQuery<Board> query = from(board); // select .. from board
 
         query.where(board.title.contains("t")); // where title like..
+
+        // 추가2, 제목, 작성자 검색 조건 추가,
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        booleanBuilder.or(board.title.contains("t"));
+        booleanBuilder.or(board.content.contains("t"));
+
+        // query , 조건을 적용하기.
+        query.where(booleanBuilder);
+        // 유효성 체크, bno 0보다 초과하는 조건,
+        query.where(board.bno.gt(0L));
+
+        // 추가1, 페이징 처리
+        this.getQuerydsl().applyPagination(pageable, query);
 
         List<Board> list = query.fetch(); // DB 서버로 호출해서, 데이터를 받아오기.
         long count = query.fetchCount(); // 조회된 데이터 갯수 확인.
