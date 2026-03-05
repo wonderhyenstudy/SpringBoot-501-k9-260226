0e2c612fdea2a8518a5cdc2a98c90584447c9701
lsy3709 <lsy3709@naver.com>
2026-03-05 10:13:13 +0900

    2교시,  글등록 작업, 화면, 로직 처리 컨트롤러 작업

--- src/main/java/com/busanit501/springboot0226/controller/BoardController.java
+++ src/main/java/com/busanit501/springboot0226/controller/BoardController.java
@@ -4,12 +4,16 @@ import com.busanit501.springboot0226.dto.BoardDTO;
 import com.busanit501.springboot0226.dto.PageRequestDTO;
 import com.busanit501.springboot0226.dto.PageResponseDTO;
 import com.busanit501.springboot0226.service.BoardService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board")
@@ -25,4 +29,30 @@ public class BoardController {
         log.info("BoardController에서, responseDTO 확인 ," + responseDTO);
         model.addAttribute("responseDTO", responseDTO);
     }
+
+    // 화면 제공
+    @GetMapping("/register")
+    public  void registerGet() {
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes) {
+        log.info("BoardController 에서, registerPost 작업중");
+
+        // 서버에서 유효성 체크를 했을 경우
+        if(bindingResult.hasErrors()) {
+            log.info("BoardController 에서, registerPost , 유효성 오류 발생. ");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/board/register";
+        }
+        // 유효성 체크를 통과한 경우.
+        log.info("boardDTO 확인 : " + boardDTO);
+        Long bno = boardService.register(boardDTO);
+        redirectAttributes.addFlashAttribute("result", bno);
+        return "redirect:/board/list";
+
+    }
+
 }
--- src/main/java/com/busanit501/springboot0226/dto/BoardDTO.java
+++ src/main/java/com/busanit501/springboot0226/dto/BoardDTO.java
@@ -1,5 +1,7 @@
 package com.busanit501.springboot0226.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,8 +16,15 @@ import java.time.LocalDateTime;
 public class BoardDTO {
 
     private Long bno;
+
+    @NotEmpty
+    @Size(min = 3, max = 100)
     private String title;
+
+    @NotEmpty
     private String content;
+
+    @NotEmpty
     private String writer;
     private LocalDateTime regDate;
     private LocalDateTime modDate;
